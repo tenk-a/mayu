6458d0ecb62f5347e23abf4aafab61ad72a373ed
gimy <>
2004-06-12 15:10:34 +0000

    fix: match result 'what' refers search target 'path', so 'path' must live after search

--- s/installer.cpp
+++ s/installer.cpp
@@ -199,10 +199,11 @@ namespace Installer
     CHECK_TRUE( GetModuleFileName(g_hInst, buf, NUMBER_OF(buf)) );
     tregex reg(_T("^(.*)\\\\[^\\\\]*$"));
     tcmatch_results what;
-    if (boost::regex_search(tstring(buf), what, reg))
+    tstringi path(buf);
+    if (boost::regex_search(path, what, reg))
       return what.str(1);
     else
-      return buf;
+      return path;
   }
 
 
