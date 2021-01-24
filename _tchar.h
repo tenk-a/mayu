11e3523115ce2fe0b6aa2f0c4be5fd46f70ca0ed
tenk-a <tenka@6809.net>
2021-01-24 09:22:22 +0900

    Changed _ismbblead () to always return false assuming UTF8

--- _tchar.h
+++ _tchar.h
@@ -1,45 +1,51 @@
 #pragma once
 
 #if defined(WIN32)
-#include <tchar.h>
+ #include <tchar.h>
 #elif defined(__GNUC__)
-
-#if defined(UNICODE)
-typedef wchar_t     _TCHAR;
-#define _T(x)       L ## x
-
-#elif defined(_MBCS)
-typedef char        _TCHAR;
-#else //contain _SBCS
-typedef char        _TCHAR;
-typedef _TCHAR      TCHAR;
-#define _tcsicmp    strcasecmp
-#define _T(x)       x
-#define _sntprintf  snprintf
-#endif
+ #if defined(UNICODE)
+  typedef wchar_t       _TCHAR;
+  #define _T(x)         L ## x
+ #elif defined(_MBCS)
+  typedef char          _TCHAR;
+ #else //contain        _SBCS
+  typedef char          _TCHAR;
+  typedef _TCHAR        TCHAR;
+  #define _tcsicmp      strcasecmp
+  #define _T(x)         x
+  #define _sntprintf    snprintf
+ #endif
 
 // _T functions
 
-// eucとsjisのみ対応 とりあえず、UTF-8とか wchar_t は後回し
 // TODO:
+#if 1
+// UTF8 環境のみ対応ということにして、文字の2バイト目以降は0x80以上なので とりあえず falseで十分...
+inline bool _ismbblead(unsigned char /*c*/)
+{
+    return false;
+}
+#else
+// eucとsjisのみ対応 とりあえず、UTF-8とか wchar_t は後回し.
 inline bool _ismbblead(unsigned char c)
 {
-    return (0xFE >= c && c >= 0xA1) // euc
-        || (0x9F >= c && c >= 0x81) // sjis
-        || (0xEF >= c && c >= 0xE0) // sjis
-        ;
+    return    (0xFE >= c && c >= 0xA1) // euc
+           || (0x9F >= c && c >= 0x81) // sjis
+           || (0xEF >= c && c >= 0xE0) // sjis
+    ;
 }
+#endif
 
-#define _istalpha(c)    isalpha(c)
-#define _istdigit(c)    isdigit(c)
-#define _istpunct(c)    ispunct(c)
-#define _istspace       isspace
-#define _tcschr         strchr
-#define _istgraph       isgraph
-#define _tcsnicmp       strncasecmp
-#define _istprint       isprint
-#define _tcstol         strtol
-#define _tfopen         fopen
-#define _tstati64       stat
-#define _istlead        _ismbblead
+ #define _istalpha(c)   isalpha(c)
+ #define _istdigit(c)   isdigit(c)
+ #define _istpunct(c)   ispunct(c)
+ #define _istspace      isspace
+ #define _tcschr        strchr
+ #define _istgraph      isgraph
+ #define _tcsnicmp      strncasecmp
+ #define _istprint      isprint
+ #define _tcstol        strtol
+ #define _tfopen        fopen
+ #define _tstati64      stat
+ #define _istlead       _ismbblead
 #endif
